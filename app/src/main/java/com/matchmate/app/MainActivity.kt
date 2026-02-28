7a607154f61b311f250ae0a8db951b5bd24c7992
Abhimanyu Mishra <abhimanyu.mishra@bharatpe.com>
2026-02-28 20:52:59 +0530

    Initial commit

---
+++ app/src/main/java/com/matchmate/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.matchmate.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.matchmate.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
